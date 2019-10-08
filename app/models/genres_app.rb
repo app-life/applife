65d83b87d0b5c4de7d44a7d16bdd95d960983b96
kaomozii <19910704sn@gmail.com>
2019-10-08 04:40:49 +0000

    finish

---
+++ app/models/genres_app.rb
@@ -0,0 +1,2 @@
+class GenresApp < ApplicationRecord
+end
---
+++ db/migrate/20191007093859_create_contacts.rb
@@ -0,0 +1,15 @@
+class CreateContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contacts do |t|
+      t.integer :user_id
+      t.string :title
+      t.text :body
+      t.integer :mail
+      t.datetime :created_at
+      t.datetime :updated_at
+
+
+      t.timestamps
+    end
+  end
+end
