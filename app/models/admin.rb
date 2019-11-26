9f5dfbc00912741d9430084739ee853272787fb5
kaomozii <19910704sn@gmail.com>
2019-11-27 00:01:57 +0900

    ID作成のため

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, #:registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     enum os_category: { iOS: 0, macOS: 1}
     enum price_category: { free: 0, pay: 1}
